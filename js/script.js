c3751fbed235167a230aece3feb54c7f20ff8ded
valera <valera.sheva@gmail.com>
2021-09-04 21:56:21 +0300

    Modals/popup notification

--- js/script.js
+++ js/script.js
@@ -36,38 +36,111 @@ window.addEventListener("DOMContentLoaded", function () {
       });
     }
   });
+
+  //Timer;
+
+  const deadline = "2021-09-11";
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / (1000 * 60)) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
+
+  // modal
+
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    // modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+
+  const modalTimerId = setTimeout(openModal, 5000);
 });
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   const tabs = document.querySelectorAll(".tabheader__item"),
-//     tabsContent = document.querySelectorAll(".tabcontent"),
-//     tabsParent = document.querySelector(".tabheader__items");
-
-//   function hideTabContent() {
-//     tabsContent.forEach((element) => {
-//       element.style.display = "none";
-//     });
-//     tabs.forEach((element) => {
-//       element.classList.remove("tabheader__item_active");
-//     });
-//   }
-
-//   function showTabContent(i = 0) {
-//     tabsContent[i].style.display = "block";
-//     tabs[i].classList.add("tabheader__item_active");
-//   }
-
-//   hideTabContent();
-//   showTabContent();
-
-//   tabsParent.addEventListener("click", (event) => {
-//     if (event.target && event.target.classList.contains("tabheader__item")) {
-//       tabs.forEach((element, i) => {
-//         if (event.target == element) {
-//           hideTabContent();
-//           showTabContent(i);
-//         }
-//       });
-//     }
-//   });
-// });
