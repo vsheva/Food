faa71daf2b66a7319c7b494450856ba801f70db4
valera <valera.sheva@gmail.com>
2021-09-06 11:10:44 +0300

    Add rest and parametrs by default

--- js/script.js
+++ js/script.js
@@ -145,13 +145,15 @@ window.addEventListener("DOMContentLoaded", function () {
   // const modalTimerId = setTimeout(openModal, 5000);
 
   // Используем классы для карточек
+
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.transfer = 27;
       this.parent = document.querySelector(parentSelector);
     }
@@ -163,17 +165,23 @@ window.addEventListener("DOMContentLoaded", function () {
     render() {
       const element = document.createElement("div");
 
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
       element.innerHTML = `
-    <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-                </div>
+              <img src=${this.src} alt=${this.alt}>
+              <h3 class="menu__item-subtitle">${this.title}</h3>
+              <div class="menu__item-descr">${this.descr}</div>
+              <div class="menu__item-divider"></div>
+              <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+              </div>
+                 
                 `;
 
       this.parent.append(element);
@@ -198,7 +206,8 @@ window.addEventListener("DOMContentLoaded", function () {
     "Меню “Премиум”",
     "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
     550,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -207,6 +216,7 @@ window.addEventListener("DOMContentLoaded", function () {
     'Меню "Постное"',
     "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
     430,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 });
